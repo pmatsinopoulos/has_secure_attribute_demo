05a447659a3b953e407be33f5f9b03334fcbf5cb
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2013-08-15 22:17:13 +0300

    Security answer case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_security_answer
+  has_secure_security_answer :case_sensitive => false
   validates :username, :presence => true, :uniqueness => {:case_sensitive => false}
   validates :security_question, :presence => true
 end
